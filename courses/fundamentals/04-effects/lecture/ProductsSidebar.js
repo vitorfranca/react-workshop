8626507df5aca733ee6f030e10b35f56d624149f
Vitor França <vitufr@gmail.com>
2020-02-25 14:50:03 -0500

    lecture 4

--- courses/fundamentals/04-effects/lecture/ProductsSidebar.js
+++ courses/fundamentals/04-effects/lecture/ProductsSidebar.js
@@ -2,7 +2,19 @@ import React, { useState, useEffect } from 'react'
 import ProductFilters from 'YesterTech/ProductFilters'
 
 function ProductsSidebar() {
-  return (
+  const query = `(min-width: 860px)`
+  const [isWide, setWide] = useState(window.matchMedia(query).matches)
+
+  // subscribe to media query
+  useEffect(() => {
+    const media = window.matchMedia(query)
+    const listener = (e) => setWide(e.matches)
+    media.addListener(listener)
+    if (media.matches) setWide(true)
+    return () => media.removeListener(listener)
+  }, [query])
+
+  return isWide && (
     <aside>
       <ProductFilters />
     </aside>
