3788e85cc2ba0d7b86c64119f5bf363478d149c1
Vitor França <vitufr@gmail.com>
2020-02-25 11:29:24 -0500

    lecture 2

--- courses/fundamentals/02-state/lecture/Quantity.js
+++ courses/fundamentals/02-state/lecture/Quantity.js
@@ -3,23 +3,33 @@ import { FaMinusCircle, FaPlusCircle } from 'react-icons/fa'
 import 'YesterTech/Quantity.scss'
 
 export default function Quantity() {
-  const quantity = 1
+  const [quantity, setQuantity] = useState(0)
+  const [error, setError] = useState(null)
+
+  const increateQuantity = () => setQuantity(quantity + 1)
+  const decreateQuantity = () => {
+    if (quantity > 0) setQuantity(quantity - 1)
+    else setError('😠You must not have a negative amount in your cart! 😠')
+  }
 
   return (
     <div className="quantity-picker">
-      <div>
-        <div>
-          <button type="button" className="icon-button">
-            <FaMinusCircle />
-          </button>
-        </div>
-        <div className="input-container">{quantity}</div>
-        <div>
-          <button type="button" className="icon-button">
-            <FaPlusCircle />
-          </button>
-        </div>
-      </div>
+      <span>
+        <button type="button" className="icon-button" onClick={decreateQuantity}>
+          <FaMinusCircle />
+        </button>
+      </span>
+      <span className="input-container">{quantity}</span>
+      <span>
+        <button type="button" className="icon-button" onClick={increateQuantity}>
+          <FaPlusCircle />
+        </button>
+      </span>
+      {error && (
+        <marquee scrollamount={15}>
+          <font size={5} color="red">{error}</font>
+        </marquee>
+      )}
     </div>
   )
 }
