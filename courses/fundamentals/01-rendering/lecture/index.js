c4927a527383f211e8190f1f2fa1c97162be4768
Vitor França <vitufr@gmail.com>
2020-02-25 10:35:17 -0500

    lecture 1

--- courses/fundamentals/01-rendering/lecture/index.js
+++ courses/fundamentals/01-rendering/lecture/index.js
@@ -4,3 +4,33 @@ import { FaRegStar, FaStar } from 'react-icons/fa'
 import './styles.scss'
 
 // let's go
+// const reactElement = <div>Hello James</div>
+const Button = ({ children }) => (
+  <button className="render_button">
+    <FaStar/>
+    {children}
+    <FaStar/>
+  </button>
+)
+const reactElement = React.createElement(
+  'button',
+  { 
+    className: 'render_button',
+    style: {
+      color: 'aliceblue',
+      backgroundColor: 'cornflowerblue',
+      border: '5px solid currentColor',
+      boxShadow: '-2px 3px 6px skyblue'
+    }
+  },
+  'Hello',
+)
+const domElement = document.getElementById('root')
+
+ReactDOM.render(
+  <div>
+    {reactElement} 
+    <Button>React</Button>
+  </div>,
+  domElement,
+)
